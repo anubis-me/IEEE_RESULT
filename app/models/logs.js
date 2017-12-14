0ae538dc7ce01409c0167ee0d3fa04b448a2ae64
Karishnu Poddar <karishnu@gmail.com>
2017-12-14 21:02:55 +0530

    logs

--- app/models/logs.js
+++ app/models/logs.js
@@ -1,10 +1,11 @@
 var mongoose = require('mongoose');            // Import Mongoose Package
-var Schema   = mongoose.Schema;                // Assign Mongoose Schema function to variable
-var titlize  = require('mongoose-title-case'); // Import Mongoose Title Case Plugin
+var Schema = mongoose.Schema;                // Assign Mongoose Schema function to variable
+var titlize = require('mongoose-title-case'); // Import Mongoose Title Case Plugin
 
 // Wish Mongoose Schema
 var logSchema = new Schema({
-    regnum     : { type: String, required: true},
+    regnum: {type: String, required: true},
+    selected: {type: Boolean, required: true},
     time: {type: Date, required: true, default: Date.now()}
 });
 
